063f1a56cfc52288e1257e132a7f8fc79895dbab
Luciano <95.luciano.alvares@gmail.com>
2018-12-05 10:52:20 -0300

    Agregar archivos de proyecto.

---
+++ ExamenPrimeraInternet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamenPrimeraInternet.MvcApplication" Language="C#" %>
---
+++ ExamenPrimeraInternet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamenPrimeraInternet.Startup))]
+namespace ExamenPrimeraInternet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
